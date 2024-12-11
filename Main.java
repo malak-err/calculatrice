0e6900090fd531ded30e520cf76a0a0f127f3964
 <erraoudymalak1997@gmail.com>
2024-12-11 11:54:10 +0100

    fonction de factorielle etmodification

--- Main.java
+++ Main.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
 public class Main {
-
     public static double addition(double a, double b) {
         return a + b;
     }
 
-    public static int soustraction(double a, double b) {
+    public static double soustraction(double a, double b) {
         return a - b;
     }
 
-    public static int multiplication(double a, double b) {
+    public static double multiplication(double a, double b) {
         return a * b;
     }
 
-    public static double  division(double a, double b) {
+    public static double division(double a, double b) {
         if (b == 0) {
             System.out.println("Erreur : veillez réssayer !");
-            return Double.NaN;
+            return 0;
         }
         return a / b;
     }
@@ -25,55 +24,76 @@ public class Main {
     public static double puissance(double a, double b) {
         return Math.pow(a, b);
     }
+
     public static double racineCarree(double a) {
         if (a < 0) {
-            System.out.println("Erreur : veillez réssayez : ");
-            return Double.NaN;
+            System.out.println("Erreur : veillez réssayez !");
         }
         return Math.sqrt(a);
+    }
+
+    public static double factorial(double a) {
+        if (a < 0) {
+            System.out.println("ERREUR : veillez réssayer !");
+        }
+        long fact = 1;
+        for (int i = 1; i <= a; i++) {
+            fact = fact * i;
+        }
+        return fact;
     }
 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int choix;
+        do {
+            System.out.print("Entrez le premier nombre : ");
+            double a = scanner.nextDouble();
 
-        System.out.print("Entrez le premier nombre : ");
-        double num1 = scanner.nextDouble();
+            System.out.print("Entrez le deuxième nombre : ");
+            double b = scanner.nextDouble();
 
-        System.out.print("Entrez le deuxième nombre : ");
-        double num2 = scanner.nextDouble();
+            System.out.println("Choisissez une opération :");
+            System.out.println("1. Addition ");
+            System.out.println("2. Soustraction ");
+            System.out.println("3. Multiplication ");
+            System.out.println("4. Division ");
+            System.out.println("5. Puissance ");
+            System.out.println("6. Racine carrée ");
+            System.out.println("7. Factorielle ");
+            System.out.print("veillez choisis une operation : ");
+            choix = scanner.nextInt();
 
-        System.out.println("Choisissez une opération :");
-        System.out.println("1. Addition ");
-        System.out.println("2. Soustraction ");
-        System.out.println("3. Multiplication ");
-        System.out.println("4. Division ");
-        System.out.println("5. Puissance ");
-        System.out.println("6. Racine carrée ");
-        System.out.println("7. Factorielle ");
-        System.out.print("veillez choisis une operation : ");
-        int choix = scanner.nextInt();
+            switch (choix) {
 
-        switch (choix) {
-            case 1:
-                System.out.println("Résultat : " + addition(a, b));
-                break;
-            case 2:
-                System.out.println("Résultat : " + soustraction(a, b));
-                break;
-            case 3:
-                System.out.println("Résultat : " + multiplication(a, b));
-                break;
-            case 4:
-                System.out.println("Résultat : " + division(a, b));
-                break;
-            case 5:
-                System.out.println("Résultat : " + puissance(a, b));
-                break;
-            case 6:
-                System.out.println("Racine carrée du premier nombre : " + racineCarree(a));
-                System.out.println("Racine carrée du deuxième nombre : " + racineCarree(b));
-                break;
-            case 7:
-
-
+                case 1:
+                    System.out.println("Résultat : " + addition(a, b));
+                    break;
+                case 2:
+                    System.out.println("Résultat : " + soustraction(a, b));
+                    break;
+                case 3:
+                    System.out.println("Résultat : " + multiplication(a, b));
+                    break;
+                case 4:
+                    System.out.println("Résultat : " + division(a, b));
+                    break;
+                case 5:
+                    System.out.println("Résultat : " + puissance(a, b));
+                    break;
+                case 6:
+                    System.out.println("Racine carrée du premier nombre : " + racineCarree(a));
+                    break;
+                case 7:
+                    System.out.println("resultat :" + factorial(a));
+                    break;
+                case 8:
+                    System.out.println("merci!");
+                    break;
+                default:
+                    System.out.println("choix invalide");
+            }
+        } while (choix != 8);
+    }
+}
